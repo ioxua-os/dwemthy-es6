9bba2f473477f43dc938ce817cdb9556764d3113
FelipeFAST <alves_felipesilva@outlook.com>
2018-12-20 22:06:47 -0200

    refactor: turns my Dwemthy's Array into a functional game

--- dwemthy.js
+++ dwemthy.js
@@ -1,93 +1,68 @@
 
 const rand = (max) => Math.floor(Math.random() * max);
 
-class Creature {
-    constructor() {
-        this.traits("life", "strength", "charisma", "weapon");
-    }
-    
-    get name() {
-        return this.constructor.name;
-    }
-    
-    traits() {
-        Array.from(arguments).forEach(name => {
-            Object.defineProperties(this, {
-                [name]: {
-                    writable: true
-                }
-            });
-        });
-    }
-    
-    /**
-    * Applies a hit taken during a fight
-    */
-    hit(damage) {
-        const prob = rand(this.charisma);
-        if (prob % 9 == 7) {
-            this.life += prob / 4;
-            console.log(`[${this.name} magick powers up ${prob}!]`);
-        }
-        
-        this.life -= damage;
-        
-        if (this.life <= 0) console.log(`[${this.name} has died.]`);
-    }
-    
-    /**
-    * This method takes one turn in a fight
-    */
-    fight(enemy, weapon = this.weapon) {
-        if (this.life <= 0) {
-            console.log(`[${this.name} is too dead to fight!]`);
-            return;
-        }
-        
-        // Attaaaaaaaaack!
-        const hit = rand(this.strength + weapon);
-        console.log(`[You hit with ${hit} points of damage!`);
-        enemy.hit(hit);
-        
-        // Retaliation
-        //console.log(enemy) // ?
-        if (enemy.life > 0) {
-            const enemyHit = rand(enemy.strength + enemy.weapon);
-            console.log(`[${enemy.name} hit with ${enemyHit} points of damage!`);
-            this.hit(enemyHit);
-        }
-    }
-}
+const creatureFactory = (name, life, strength, charisma, weapon, customProps) => function create() { 
+    return {
+        name, 
+        life, 
+        strength, 
+        charisma, 
+        weapon, 
+        charisma,
 
-// Our hero:
-class Rabbit extends Creature {
-    constructor() {
-        super();
-        this.traits("bombs");
-        this.life = 10;
-        this.strength = 2;
-        this.charisma = 44;
-        this.weapon = 4;
-        this.bombs = 3;
+        hit(damage) {
+            const prob = rand(this.charisma);
+            if (prob % 9 == 7) {
+                this.life += prob / 4;
+                console.log(`[${this.name} magick powers up ${prob}!]`);
+            }
+            
+            this.life -= damage;
+            
+            if (this.life <= 0) console.log(`[${this.name} has died.]`);
+        },
+
+        fight(enemy, weapon = this.weapon) {
+            if (this.life <= 0) {
+                console.log(`[${this.name} is too dead to fight!]`);
+                return;
+            }
+            
+            // Attaaaaaaaaack!
+            const hit = rand(this.strength + weapon);
+            console.log(`[You hit with ${hit} points of damage!`);
+            enemy.hit(hit);
+            
+            // Retaliation
+            //console.log(enemy) // ?
+            if (enemy.life > 0) {
+                const enemyHit = rand(enemy.strength + enemy.weapon);
+                console.log(`[${enemy.name} hit with ${enemyHit} points of damage!`);
+                this.hit(enemyHit);
+            }
+        },
+
+        // Custom props go here to simulate inheritance
+        ...customProps
     }
-    
-    // Unfortunately, there can be no special characters on method names in JS
-    // We can use something like creature["^"](enemy) but it's ugly
+};
+
+// Our hero
+const Rabbit = creatureFactory("Rabbit", 10, 2, 44, 4, {
+    bombs: 3,
+
     boomerang(enemy) {
         this.fight(enemy, 13);
-    }
-    
+    },
     sword(enemy) {
         this.fight(enemy, rand(4 + Math.pow(enemy.life % 10, 2)));
-    }
-    
+    },
     lettuce(enemy) {
         const lettuce = rand(this.charisma);
         console.log(`[Healthy lettuce gives you ${lettuce} life points!!]`);
         this.life += lettuce;
         fight(enemy, 0);
-    }
-    
+    },
     bomb(enemy) {
         if (this.bombs == 0) {
             console.log("[UHN!! You're out of bombs!!]");
@@ -97,71 +72,18 @@ class Rabbit extends Creature {
         this.bombs -= 1;
         this.fight(enemy, 86);
     }
-}
+});
 
 // Our enemies
-class IndustrialRaverMonkey extends Creature {
-    constructor() {
-        super();
-        this.life = 46;
-        this.strength = 35;
-        this.charisma = 91;
-        this.weapon = 2;
-    }
-}
+const IndustrialMonkey = creatureFactory("IndustrialMonkey", 46, 35, 91, 2);
+const DwarvenAngel = creatureFactory("DwarvenAngel", 540, 6, 144, 58);
+const AssistantViceOmbudsman = creatureFactory("AssistantViceOmbudsman", 320, 6, 144, 50);
+const TeethDeer = creatureFactory("TeethDeer", 655, 192, 19, 109);
+const IntrepidCyclist = creatureFactory("IntrepidCyclist", 901, 560, 422, 105);
+const Dragon = creatureFactory("Dragon", 1340, 451, 1020, 939);
 
-class DwarvenAngel extends Creature {
-    constructor() {
-        super();
-        this.life = 540;
-        this.strength = 6;
-        this.charisma = 144;
-        this.weapon = 58;
-    }
-}
-
-class AssistantViceTentacleAndOmbudsman extends Creature {
-    constructor() {
-        super();
-        this.life = 320;
-        this.strength = 6;
-        this.charisma = 144;
-        this.weapon = 50;
-    }
-}
-
-class TeethDeer extends Creature {
-    constructor() {
-        super();
-        this.life = 655;
-        this.strength = 192;
-        this.charisma = 19;
-        this.weapon = 109;
-    }
-}
-
-class IntrepidDecomposedCyclist extends Creature {
-    constructor() {
-        super();
-        this.life = 901;
-        this.strength = 560;
-        this.charisma = 422;
-        this.weapon = 105;
-    }
-}
-
-class Dragon extends Creature {
-    constructor() {
-        super();
-        this.life = 1340;
-        this.strength = 451;
-        this.charisma = 1020;
-        this.weapon = 939;
-    }
-}
-
-const dwemthysArrayOf = function() {
-    return new Proxy(Array.from(arguments), {
+const dwemthysArrayOf = (...creatures) => {
+    return new Proxy(creatures, {
         get: function(target, prop, _) {
             if (target.length == 0) {
                 console.log("[Whoa.  You decimated Dwemthy's Array!]");
@@ -188,11 +110,11 @@ const dwemthysArrayOf = function() {
 
 // And then.. SHALL THE CARNAGE BEGIN!
 const dwarr = dwemthysArrayOf(
-        new IndustrialRaverMonkey(),
+        new IndustrialMonkey(),
         new DwarvenAngel(),
-        new AssistantViceTentacleAndOmbudsman(),
+        new AssistantViceOmbudsman(),
         new TeethDeer(),
-        new IntrepidDecomposedCyclist(),
+        new IntrepidCyclist(),
         new Dragon());
 
 const aliveRabbit = new Rabbit();
